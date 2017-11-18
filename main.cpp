bcc95da53c6d76c61c37be8a9942bec0ac3b8891
Wei <10310932@std.tcfsh.tc.edu.tw>
2017-11-18 14:37:17 +0800

    Probably the final version

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <stack>
 #include <cstdlib>
 #include <vector>
+#include <windows.h>
 
 using namespace std;
 
@@ -57,7 +58,7 @@ public:
 		score = 0;
 		for (int i = 0; i<3; i++) {
 			int tmp = sorted[i];
-			if(tmp<7)score += (1 + tmp)*tmp / 2;
+			if (tmp<7)score += (1 + tmp)*tmp / 2;
 			else score += 21;
 		}
 		for (int i = 3; i<7; i++) {
@@ -86,8 +87,9 @@ public:
 		for (int i = 2; i<9; i++) if (cards[i]>tmp)tmp = cards[i];
 		return tmp;
 	}
-	void clear() {
+	void _clear() {
 		for (int i = 0; i<9; i++) cards[i] = 0;
+		isover = false;
 	}
 private:
 	int cards[9];
@@ -147,7 +149,7 @@ private:
 	bool is_taken;
 };
 
-void AI_processing(Player players[], on_table_deck on_deck[], int cur_player, int player_num, int state, stack<int>& main_deck, int &state_code, bool &end_game) {
+void AI_processing(Player players[], on_table_deck on_deck[], int cur_player, int player_num, int state, stack<int>& main_deck, int &state_code, bool &end_game, bool is_AI[]) {
 	vector<int> my_card = players[cur_player].all_cards();
 	vector< vector <int> > oppo_card(player_num - 1, vector<int>());
 	vector<int> take_deck_expectation(player_num, 0);
@@ -156,7 +158,7 @@ void AI_processing(Player players[], on_table_deck on_deck[], int cur_player, in
 	for (int i = 0; i<player_num - 1; i++) oppo_card[i] = players[(cur_player + i) % player_num].all_cards();
 	if (state == 1) {
 		for (int i = 0; i<player_num; i++) {
-			if (on_deck[i].istaken()||on_deck[i].isempty()) {
+			if (on_deck[i].istaken() || on_deck[i].isempty()) {
 				take_deck_expectation[i] = -1;
 			}
 			else {
@@ -195,11 +197,11 @@ void AI_processing(Player players[], on_table_deck on_deck[], int cur_player, in
 					break;
 				}
 			}
-			if (end_game ==true) {
+			if (end_game == true) {
 				cout << "The game will end after this round" << endl << endl;
 			}
-			cout << "Player " << cur_player + 1 << " has taken deck " << tmp + 1 << "." << endl;
-			system("PAUSE");
+			cout << "Player " << cur_player + 1 << (is_AI[cur_player] ? "(AI)" : "") << " has taken deck " << tmp + 1 << "." << endl;
+			system("PAUSE>nul");
 			state_code = is_all_taken ? 7 : 6;
 			return;
 		}
@@ -216,16 +218,16 @@ void AI_processing(Player players[], on_table_deck on_deck[], int cur_player, in
 			on_deck[deck_num].putcard(tmp_card);
 			main_deck.pop();
 			state_code = 6;
-			cout << "Player " << cur_player + 1 << " has drawn ";
+			cout << "Player " << cur_player + 1 << (is_AI[cur_player] ? "(AI)" : "") << " has drawn ";
 			print_card(tmp_card);
-			cout<<endl<<endl<< "Player " << cur_player + 1 << " has put it to deck "<<deck_num+1;
-			system("PAUSE");
+			cout << endl << "Player " << cur_player + 1 << (is_AI[cur_player] ? "(AI)" : "") << " has put it to deck " << deck_num + 1<<endl;
+			system("PAUSE>nul");
 			return;
 		}
 	}
 	if (state == 2) {
 		for (int i = 0; i<player_num; i++) {
-			if (on_deck[i].istaken()||on_deck[i].isempty()) {
+			if (on_deck[i].istaken() || on_deck[i].isempty()) {
 				take_deck_expectation[i] = -1;
 			}
 			else {
@@ -266,14 +268,14 @@ void AI_processing(Player players[], on_table_deck on_deck[], int cur_player, in
 		if (end_game == true) {
 			cout << "The game will end after this round" << endl << endl;
 		}
-		cout << "Player " << cur_player + 1 << " has taken deck " << tmp + 1 << "." << endl;
-		system("PAUSE");
+		cout << "Player " << cur_player + 1 << (is_AI[cur_player] ? "(AI)" : "") << " has taken deck " << tmp + 1 << "." << endl;
+		system("PAUSE>nul");
 		state_code = is_all_taken ? 7 : 6;
 		return;
 	}
 	else {
 		srand(time(NULL));
-		int tmp_card = main_deck.top(), deck_num=rand() % player_num;
+		int tmp_card = main_deck.top(), deck_num = rand() % player_num;
 		if (main_deck.size() <= 15) {
 			end_game = true;
 			cout << "The game will end after this round" << endl << endl;
@@ -284,10 +286,10 @@ void AI_processing(Player players[], on_table_deck on_deck[], int cur_player, in
 		on_deck[deck_num].putcard(tmp_card);
 		main_deck.pop();
 		state_code = 6;
-		cout << "Player " << cur_player + 1 << " has drawn ";
+		cout << "Player " << cur_player + 1 << (is_AI[cur_player] ? "(AI)" : "") << " has drawn ";
 		print_card(tmp_card);
-		cout << endl << endl << "Player " << cur_player + 1 << " has put it to deck " << deck_num + 1;
-		system("PAUSE");
+		cout  << endl << "Player " << cur_player + 1 << (is_AI[cur_player] ? "(AI)" : "") << " has put it to deck " << deck_num + 1<<endl;
+		system("PAUSE>nul");
 		return;
 	}
 }
@@ -298,12 +300,13 @@ void converting(int &integer, char raw_input[], int &state_code, int player_num 
 		return;
 	}
 	else if ((raw_input[0] == 'R' || raw_input[0] == 'r') && (raw_input[1] == 'u' || raw_input[1] == 'U') && (raw_input[2] == 'l' || raw_input[2] == 'L') && (raw_input[3] == 'e' || raw_input[3] == 'E') && (raw_input[4] == 0)) {
+		system("cls");
 		cout << "You can choose either to draw a card, or take a deck on the table." << endl << "If you want to draw a card, then pick a deck which is not full or taken to rest the card." << endl
 			<< "As for score counting, every the nth coloretto of the same color will be counted as n points. For example, 5 gray coloretto is counted as 1+2+3+4+5=15 points." << endl
 			<< "Only the three colors with more number of colorettoes will be counted as plus point to the total score; other colors will be counted as minus points" << endl
 			<< "A multi-color coloretto can be counted as any kind of coloretto, the system will automatically pick the one with highest point" << endl
 			<< "Also, a +2 card can give you 2 points as the card shows." << endl << endl;
-		system("PAUSE");
+		system("PAUSE>nul");
 		system("CLS");
 		return;
 	}
@@ -313,6 +316,7 @@ void converting(int &integer, char raw_input[], int &state_code, int player_num 
 		return;
 	}
 	else if ((raw_input[0] == 'h' || raw_input[0] == 'H') && raw_input[1] == 0) {
+		system("cls");
 		if (state_code == 1) {
 			cout << "Type an integer in the range of 3~5 or\n";
 		}
@@ -394,7 +398,7 @@ void taking_cards(on_table_deck on_deck[], Player players[], int& state_code, in
 	}
 }
 
-void draw_card(on_table_deck on_deck[], int& state_code, stack<int>& main_deck, int cur_player, int player_num, bool& end_game, Player players[]) {
+void draw_card(on_table_deck on_deck[], int& state_code, stack<int>& main_deck, int cur_player, int player_num, bool& end_game, Player players[], bool is_AI[]) {
 	int tmp_card = main_deck.top();
 	int deck_num;
 	char raw_input[20];
@@ -403,16 +407,17 @@ void draw_card(on_table_deck on_deck[], int& state_code, stack<int>& main_deck, 
 		end_game = true;
 		cout << "The game will end after this round" << endl << endl;
 	}
-	cout << "Player " << cur_player + 1 << " has drawn a ";
+	cout << "Player " << cur_player + 1 << (is_AI[cur_player] ? "(AI)" : "") << " has drawn a ";
 	print_card(tmp_card);
 	cout << endl << endl;
 	for (int i = 0; i<player_num; i++) {
 		cout << "Deck" << i + 1 << ": ";
 		on_deck[i].print();
 	}
-	cout << endl;
+	for (int i = 0; i < 70; i++) cout << "_";
+	cout << endl<<endl;
 	for (int i = 0; i<player_num; i++) {
-		cout << ((players[i].is_over()) ? "*" : "") << "Player" << i + 1 << ':' << endl;
+		cout << ((players[i].is_over()) ? "[X]" : "[O]") << "Player" << i + 1 << (is_AI[cur_player] ? "(AI)" : "") << ':' << endl;
 		players[i].print_card(player_num);
 	}
 	cout << endl;
@@ -441,7 +446,12 @@ void draw_card(on_table_deck on_deck[], int& state_code, stack<int>& main_deck, 
 void print_card(int i) {
 	switch (i) {
 	case 0:
-		cout << "Multi\t";
+		cout << "M";
+		cout << "u";
+		cout << "l";
+		cout << "t";
+		cout << "i";
+		cout << "\t";
 		break;
 	case 1:
 		cout << "+2\t";
@@ -479,7 +489,9 @@ int main()
 	Player players[5];
 	on_table_deck on_deck[5];
 	bool end_game, is_AI[5];
-	cout << "Welcome to Coloretto~\n\"H\" for help\n\"Rule\" to show rules\n\"Replay\" to restart a new game\n\"Q\" to quit the game" << endl;
+	cout << "Welcome to Coloretto~\n\"H\" for help\n\"Rule\" to show rules\n\"Replay\" to restart a new game\n\"Q\" to quit the game" << endl ;
+	for (int i = 0; i < 70; i++) cout << "_";
+	cout << endl<<endl;
 	while (state_code != 0) {
 		if (state_code == 1) {
 			cout << "Please tell the host how many players are there?";
@@ -488,8 +500,9 @@ int main()
 			end_game = false;
 			for (int i = 0; i < 5; i++) {
 				on_deck[i].putback();
-				players[i].clear();
+				players[i]._clear();
 			}
+			cur_player = 0;
 		}
 		if (state_code == 11) {
 			cout << "Please tell the host how many AIs are there?";
@@ -504,11 +517,15 @@ int main()
 		}
 		if (state_code == 2) {
 			system("CLS");
-			cout << "Initial state:" << endl;
+			cout << "Initial state:" << endl ;
+			for (int i = 0; i < 70; i++) cout << "_";
+			cout << endl<<endl;
 			for (int i = 0; i<player_num; i++) {
 				cout << "Deck" << i + 1 << ": ";
 				on_deck[i].print();
 			}
+			for (int i = 0; i < 70; i++) cout << "_";
+			cout << endl<<endl;
 			int cnt_all[9] = { 3, 10, 9, 9, 9, 9, 9, 9, 9 }, cnt_draw[9] = { 0,0,1,1,1,1,1,1,1 };
 			srand(time(NULL));
 			if (player_num>3) {
@@ -523,7 +540,6 @@ int main()
 					AI_card_counting[tmp]--;
 					players[i].add_card(tmp);
 				}
-
 			}
 			else if (player_num == 3) {
 				AI_card_counting[0] = 3; AI_card_counting[1] = 10; for (int i = 2; i<8; i++) AI_card_counting[i] = 9;
@@ -539,7 +555,7 @@ int main()
 				}
 			}
 			for (int i = 0; i<player_num; i++) {
-				cout << ((players[i].is_over()) ? "*" : "") << "Player" << i + 1 << ':' << endl;
+				cout << ((players[i].is_over()) ? "[X]" : "[O]") << "Player" << i + 1 << (is_AI[cur_player] ? "(AI)" : "") << ":" << endl;
 				players[i].print_card(player_num);
 			}
 			cout << endl;
@@ -563,7 +579,7 @@ int main()
 					cnt_all[tmp]--;
 				}
 			}
-			system("PAUSE");
+			system("PAUSE>nul");
 			state_code++;
 		}
 		if (state_code == 3) {//gaming stage start from here
@@ -589,24 +605,29 @@ int main()
 			}
 			if (is_all_full) {
 				system("CLS");
-				cout << "Time for player " << cur_player + 1 << endl << endl;
+				cout << "Time for player " << cur_player + 1 << (is_AI[cur_player] ? "(AI)" : "") << endl ;
+				for (int i = 0; i < 70; i++) cout << "_";
+				cout << endl << endl;
 				cout << "All decks on the table are full. You can only take a deck" << endl << endl;
 				for (int i = 0; i<player_num; i++) {
 					cout << "Deck" << i + 1 << ": ";
 					on_deck[i].print();
 				}
-				cout << endl << endl;
+				for (int i = 0; i < 70; i++) cout << "_";
+				cout << endl<<endl;
 				for (int i = 0; i<player_num; i++) {
-					cout << ((players[i].is_over()) ? "*" : "") << "Player" << i + 1 << ':' << endl;
+					cout << ((players[i].is_over()) ? "[X]" : "[O]") << "Player" << i + 1 << (is_AI[cur_player] ? "(AI)" : "") << ':' << endl;
 					players[i].print_card(player_num);
 				}
 				state_code = 4;
 			}
 			else if (is_all_empty) {
 				system("CLS");
-				cout << "Time for player " << cur_player + 1 << endl << endl;
+				cout << "Time for player " << cur_player + 1 << (is_AI[cur_player] ? "(AI)" : "") << endl;
+				for (int i = 0; i < 70; i++) cout << "_";
+				cout << endl << endl;
 				cout << "All decks on the table are empty. \nDrawing a card..." << endl << endl;
-				system("PAUSE");
+				system("PAUSE>nul");
 				system("CLS");
 				state_code = 5;
 			}
@@ -617,14 +638,17 @@ int main()
 			else {
 				if (is_AI[cur_player] == false) {
 					system("CLS");
-					cout << "Time for player " << cur_player + 1 << endl << endl;
+					cout << "Time for player " << cur_player + 1 << (is_AI[cur_player] ? "(AI)" : "") << endl;
+					for (int i = 0; i < 70; i++) cout << "_";
+					cout << endl << endl;
 					for (int i = 0; i<player_num; i++) {
 						cout << "Deck" << i + 1 << ": ";
 						on_deck[i].print();
 					}
-					cout << endl << endl;
+					for (int i = 0; i < 70; i++) cout << "_";
+					cout  << endl<<endl;
 					for (int i = 0; i<player_num; i++) {
-						cout << ((players[i].is_over()) ? "*" : "") << "Player" << i + 1 << ':' << endl;
+						cout << ((players[i].is_over()) ? "[X]" : "[O]") << "Player" << i + 1 << (is_AI[cur_player] ? "(AI)" : "") << ':' << endl;
 						players[i].print_card(player_num);
 					}
 					cout << "What would you like to do?\nTake a deck with \'t\' key or draw a card with \'d\' key" << endl;
@@ -633,32 +657,44 @@ int main()
 						state_code = 4;
 					else if ((raw_input[0] == 'D' || raw_input[0] == 'd') && raw_input[1] == 0) {
 						system("cls");
-						cout << "Time for player " << cur_player + 1 << endl << endl << "Drawing a card..." << endl;
-						system("PAUSE");
+						cout << "Time for player " << cur_player + 1 << (is_AI[cur_player] ? "(AI)" : "") << endl;
+						for (int i = 0; i < 70; i++) cout << "_";
+						cout << endl << endl;
+						cout << "Drawing a card..." << endl;
+						system("PAUSE>nul");
 						state_code = 5;
 					}
 					else {
 						cout << "Please tell the host what do you want to do~" << endl;
-						system("PAUSE");
+						system("PAUSE>nul");
 					}
 				}
 				else {
-					AI_processing(players, on_deck, cur_player, player_num, 1, deck, state_code, end_game);
+					cout << "Time for player " << cur_player + 1 << (is_AI[cur_player] ? "(AI)" : "") << endl ;
+					for (int i = 0; i < 70; i++) cout << "_";
+					cout << endl << endl;
+					AI_processing(players, on_deck, cur_player, player_num, 1, deck, state_code, end_game, is_AI);
 				}
 			}
 		}
 		if (state_code == 4) {//take stage
 			if (is_AI[cur_player] == false) taking_cards(on_deck, players, state_code, cur_player, player_num, end_game);
 			else {
-				AI_processing(players, on_deck, cur_player, player_num, 2, deck, state_code, end_game);
+				cout << "Time for player " << cur_player + 1 << (is_AI[cur_player] ? "(AI)" : "") << endl ;
+				for (int i = 0; i < 70; i++) cout << "_";
+				cout << endl << endl;
+				AI_processing(players, on_deck, cur_player, player_num, 2, deck, state_code, end_game, is_AI);
 			}
 		}
 		if (state_code == 5) {//draw stage
 			if (is_AI[cur_player] == false) {
-				draw_card(on_deck, state_code, deck, cur_player, player_num, end_game, players);
+				draw_card(on_deck, state_code, deck, cur_player, player_num, end_game, players, is_AI);
 			}
 			else {
-				AI_processing(players, on_deck, cur_player, player_num, 3, deck, state_code, end_game);
+				cout << "Time for player " << cur_player + 1 << (is_AI[cur_player] ? "(AI)" : "") << endl ;
+				for (int i = 0; i < 70; i++) cout << "_";
+				cout << endl << endl;
+				AI_processing(players, on_deck, cur_player, player_num, 3, deck, state_code, end_game, is_AI);
 			}
 		}
 		if (state_code == 6) {
@@ -671,27 +707,31 @@ int main()
 			state_code = 3;
 			cout << endl;
 			system("CLS");
-			cout << "State for now:" << endl << endl;
+			cout << "State for now:\t" << deck.size() << "cards left" << endl;
+			for (int i = 0; i < 70; i++) cout << "_";
+			cout << endl<<endl;
 			for (int i = 0; i<player_num; i++) {
 				cout << "Deck" << i + 1 << ": ";
 				on_deck[i].print();
 			}
-			cout << endl << "Scores:\t";
-			for (int i = 0; i<player_num; i++) cout << "Player" << i + 1 << ": " << players[i].counting_score() << '\t';
-			cout << endl << endl;
+			for (int i = 0; i < 70; i++) cout << "_";
+			cout << endl <<endl<< "Scores:\n";
+			for (int i = 0; i<player_num; i++) cout << "Player" << i + 1 << (is_AI[cur_player] ? "(AI)" : "") << ": " << players[i].counting_score() << '\n';
+			for (int i = 0; i < 70; i++) cout << "_";
+			cout << endl<<endl ;
 			for (int i = 0; i<player_num; i++) {
-				cout << ((players[i].is_over()) ? "*" : "") << "Player" << i + 1 << ':' << endl;
+				cout << ((players[i].is_over()) ? "[X]" : "[O]") << "Player" << i + 1 << (is_AI[cur_player] ? "(AI)" : "") << ':' << endl;
 				players[i].print_card(player_num);
 			}
 			cout << endl;
-			system("PAUSE");
+			system("PAUSE>nul");
 
 		}
 		if (state_code == 7) {
 			system("CLS");
 			if (end_game) {
 				cout << "Here comes the result~:" << endl;
-				for (int i = 0; i<player_num; i++) cout << "Player" << i + 1 << ": " << players[i].counting_score() << '\t';
+				for (int i = 0; i<player_num; i++) cout << "Player" << i + 1 << (is_AI[cur_player] ? "(AI)" : "") << ": " << players[i].counting_score() << '\n';
 				state_code = 8;
 			}
 			else {
@@ -700,26 +740,33 @@ int main()
 					players[i].set_restart();
 					on_deck[i].putback();
 				}
-				cout << "A round has ended:" << endl << endl << "Scores:" << endl;
-				for (int i = 0; i<player_num; i++) cout << "Player" << i + 1 << ": " << players[i].counting_score() << '\t';
-				cout << endl << endl << "Cards:" << endl;
+				cout << "A round has ended:" << endl;
+				for (int i = 0; i < 70; i++) cout << "_";
+				cout << endl << endl;
+				cout<< "Scores:" << endl;
+				for (int i = 0; i<player_num; i++) cout << "Player" << i + 1 << (is_AI[cur_player] ? "(AI)" : "") << ": " << players[i].counting_score() << '\n';
+				cout << endl;
+				for (int i = 0; i < 70; i++) cout << "_";
+				cout << endl << endl;
+				cout << "Cards:" << endl;
 				for (int i = 0; i<player_num; i++) {
-					cout << "Player" << i + 1 << ':' << endl;
+					cout << "Player" << i + 1 << (is_AI[cur_player] ? "(AI)" : "") << ':' << endl;
 					players[i].print_card(player_num);
 				}
 			}
-			system("PAUSE");
+			system("PAUSE>nul");
 		}
 		if (state_code == 8) {
 			system("CLS");
 			cout << "Would you like to play another round?" << endl;
 			cin >> raw_input;
+			while ((raw_input[0] != 'y' && raw_input[0] != 'Y'&&raw_input[0] != 'n' && raw_input[0] == 'N')||raw_input[1]!=0) {
+				cout << "Please enter Y or N to tell the host whether you're willing to restart the game" << endl;
+				cout << "Would you like to play another round?" << endl;
+				cin >> raw_input;
+			}
 			if ((raw_input[0] == 'y' || raw_input[0] == 'Y') && raw_input[1] == 0) state_code = 1;
 			else if ((raw_input[0] == 'n' || raw_input[0] == 'N') && raw_input[1] == 0) state_code = 0;
-			else {
-				cout << "Please enter Y or N to tell the host your will to restart the game" << endl;
-				state_code = 8;
-			}
 		}
 		if (state_code == 9) {
 			int tmp_cnt = 0;
@@ -741,7 +788,7 @@ int main()
 			if (state_code == 9) {
 				cout << "restarting..." << endl;
 				state_code = 1;
-				system("PAUSE");
+				system("PAUSE>nul");
 				system("CLS");
 			}
 		}
@@ -752,7 +799,7 @@ int main()
 			break;
 		case 42:
 			cout << "What a pity~\nThe deck you want to take has been taken~" << endl;
-			system("PAUSE");
+			system("PAUSE>nul");
 			state_code = 4;
 			break;
 		case 43:
@@ -761,32 +808,33 @@ int main()
 			break;
 		case 44:
 			state_code = 4;
-			system("PAUSE");
+			system("PAUSE>nul");
 			break;
 		case 51:
 			cout << "Sorry, the range should be in 1~" << player_num << endl;
-			system("PAUSE");
+			system("PAUSE>nul");
 			system("CLS");
 			state_code = 5;
 			break;
 		case 52:
 			cout << "The deck is full. Choose another one please~" << endl;
-			system("PAUSE");
+			system("PAUSE>nul");
 			state_code = 5;
 			break;
 		case 53:
 			cout << "The deck is taken. Please choose another one~" << endl;
-			system("PAUSE");
+			system("PAUSE>nul");
 			system("CLS");
 			state_code = 5;
 			break;
 		case 54:
 			state_code = 5;
-			system("PAUSE");
+			system("PAUSE>nul");
 			break;
 		}
 	}
+	system("cls");
 	cout << "Thank you for playing~" << endl;
-	system("PAUSE");
+	system("PAUSE>nul");
 	return 0;
 }
